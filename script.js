8c64f38fdc4855fba2032a2e3c434b5425c0e1a3
sm67866 <sm67866@gmail.com>
2026-06-21 15:45:22 -0400

    Update script.js

--- script.js
+++ script.js
@@ -246,3 +246,58 @@ function getSpreadName() {
 
     return "One Card";
 }
+
+
+function getCardImage(cardName) {
+    const majorArcana = {
+        "The Fool": "00-TheFool.jpg",
+        "The Magician": "01-TheMagician.jpg",
+        "The High Priestess": "02-TheHighPriestess.jpg",
+        "The Empress": "03-TheEmpress.jpg",
+        "The Emperor": "04-TheEmperor.jpg",
+        "The Hierophant": "05-TheHierophant.jpg",
+        "The Lovers": "06-TheLovers.jpg",
+        "The Chariot": "07-TheChariot.jpg",
+        "Strength": "08-Strength.jpg",
+        "The Hermit": "09-TheHermit.jpg",
+        "Wheel of Fortune": "10-WheelOfFortune.jpg",
+        "Justice": "11-Justice.jpg",
+        "The Hanged Man": "12-TheHangedMan.jpg",
+        "Death": "13-Death.jpg",
+        "Temperance": "14-Temperance.jpg",
+        "The Devil": "15-TheDevil.jpg",
+        "The Tower": "16-TheTower.jpg",
+        "The Star": "17-TheStar.jpg",
+        "The Moon": "18-TheMoon.jpg",
+        "The Sun": "19-TheSun.jpg",
+        "Judgement": "20-Judgement.jpg",
+        "The World": "21-TheWorld.jpg"
+    };
+
+    if (majorArcana[cardName]) {
+        return `images/${majorArcana[cardName]}`;
+    }
+
+    const cardNumbers = {
+        "Ace": "01",
+        "Two": "02",
+        "Three": "03",
+        "Four": "04",
+        "Five": "05",
+        "Six": "06",
+        "Seven": "07",
+        "Eight": "08",
+        "Nine": "09",
+        "Ten": "10",
+        "Page": "11",
+        "Knight": "12",
+        "Queen": "13",
+        "King": "14"
+    };
+
+    const parts = cardName.split(" of ");
+    const number = cardNumbers[parts[0]];
+    const suit = parts[1];
+
+    return `images/${suit}${number}.jpg`;
+}
